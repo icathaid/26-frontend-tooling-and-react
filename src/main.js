aae446aadcdc5ecac0b3f7dc1ce3e40136a95816
James McDaniel <icathaid@gmail.com>
2018-07-30 18:13:39 -0700

    started working on setup

--- src/main.js
+++ src/main.js
@@ -1,2 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import './style/app.scss';
+
+class App extends React.component {
+    render () {
+        return 'Hola';
+    }
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
