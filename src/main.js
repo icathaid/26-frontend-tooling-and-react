0615e0bd571d572a8b6d757aae9a16348668624f
James McDaniel <icathaid@gmail.com>
2018-08-27 15:14:53 -0700

    added random number simulator and platform for testing api responses

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import cowsay, {say} from "cowsay";
 import faker from "faker";
-
+import superagent from "superagent";
 
 import './style/app.scss';
+
+const pokemonAPI = 'https://www.pokeapi.co/api/v2/pokemon/';
 
 class App extends Component {
     constructor(props){
@@ -13,6 +15,7 @@ class App extends Component {
             content: say({
                 text: faker.fake("({{lorem.sentence}})")
             }),
+            
         };
         this.fakeStuff = this.fakeStuff.bind(this);
     }
@@ -24,11 +27,35 @@ class App extends Component {
         const content = say({text:lorem});
         this.setState({content});
     }
+    randomNumber(){
+        let result = Math.floor((Math.random() * 100) + 1);
+        if(result > 57){
+            return alert('LOSS');
+        }
+        else return alert('WIN');
+    }
+    testAPI(){
+        let bill = {
+            "key":"value",
+            'foo':'bar',
+            test:42
+        }
+        let search = 'ivysaur';
+        let url = `${pokemonAPI}${search}`;
+        let ted = superagent.get(url)
+            .then(result => {
+                return result.body;
+            });
+
+        return console.log(ted.value);
+    }
     render() {
         return <div className="App">
             <h1>Generate Cowsay Lorem</h1>
             {this.getContent()}
             <button onClick={this.fakeStuff}>Click Me</button>
+            <button onClick={this.randomNumber}>Random</button>
+            <button onClick={this.testAPI}>API</button>
         </div>
     }
 }
